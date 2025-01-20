99b44303926a73de105e8440c5be77bae4e8b77c
linedj <eunjun998@gmail.com>
2025-01-20 14:01:47 +0900

    application run transaction apply

--- src/main/java/com/example/jpa/domain/post/post/service/PostService.java
+++ src/main/java/com/example/jpa/domain/post/post/service/PostService.java
@@ -4,8 +4,9 @@ import com.example.jpa.domain.post.post.entity.Post;
 import com.example.jpa.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,17 +16,37 @@ public class PostService {
 
     public Post write(String title, String body) {
 
-        Post post = new Post(
-                null,
-                LocalDateTime.now(),
-                LocalDateTime.now(),
-                title,
-                body);
+        Post post = Post.builder()
+                .title(title)
+                .body(body)
+                .build();
 
         return postRepository.save(post);
     }
 
-    public long count(){
+    @Transactional
+    public Post modify(Post post, String title, String body) {
+
+        post.setTitle(title);
+        post.setBody(body);
+
+        return post;
+    }
+
+    public long count() {
         return postRepository.count();
+    }
+
+    public Optional<Post> findById(long id) {
+        return postRepository.findById(id);
+    }
+
+
+    @Transactional // 메서드의 시작이 트랜잭션 시작. 메서드의 끝이 트랜잭션 종료.
+    public void modify2(long id, String title, String body) {
+        Post post = postRepository.findById(id).get();
+
+        post.setTitle(title);
+        post.setBody(body);
     }
 }
--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -1,10 +1,14 @@
 package com.example.jpa.global;
 
+import com.example.jpa.domain.post.post.entity.Post;
 import com.example.jpa.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
@@ -13,20 +17,42 @@ public class BaseInitData {
     private final PostService postService;
 
     @Bean
+    @Order(1)
     public ApplicationRunner applicationRunner() {
         return args -> {
             // 샘플 데이터 3개 생성.
-
+            // 데이터가 3개가 이미 있으면 패스
             if( postService.count() > 0 ) {
                 return ;
             }
 
             postService.write("title1", "body1");
-
-            postService.write("title2", "body3");
-
+            postService.write("title2", "body2");
             postService.write("title3", "body3");
 
+        };
+    }
+
+    @Bean
+    @Order(2)
+    public ApplicationRunner applicationRunner2() {
+//        return args -> {
+//            Post post = postService.findById(1L).get();
+//
+//            Thread.sleep(1000);
+//
+//            postService.modify(post, "new title", "new body");
+////            postService.modify2(1L, "new title2", "new body2");
+//        };
+
+        return new ApplicationRunner() {
+            @Override
+            @Transactional
+            public void run(ApplicationArguments args) throws Exception {
+                Post post = postService.findById(1L).get();
+                Thread.sleep(1000);
+                postService.modify(post, "new title1212", "new body1212");
+            }
         };
     }
 
